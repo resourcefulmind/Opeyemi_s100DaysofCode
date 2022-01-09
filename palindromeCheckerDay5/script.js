3db3baa07d7a7604a407a643493522b19b7af723
devVGBG <resourcefulmindoso@gmail.com>
2022-01-09 15:43:17 +0100

    add a little part of  palindrome script logic

---
+++ palindromeCheckerDay5/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", palindrome);
+
+
+function palindrome() {
+    //declare a variable "checkWord" and assign the queryselector that checks the value of the targeted class 
+    const checkWord = document.querySelector(".input-text").value;
+    
+}
